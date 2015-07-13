0ce80bbb951eeeb2084ac5993d202f7ca0627275
Michael Von Hippel <michaelvonhippel@lbl.gov>
2015-07-13 14:54:15 -0700

    Hue drivers, not yet debugged, since API not yet on vagrant system. Will follow with EUD wrapper for Hue.

---
+++ hue_control/hue_control.py
@@ -0,0 +1,71 @@
+from beautifulhue import Bridge
+
+# Control functionality of bridge system wrapper to simplify interface
+class HueBridge:
+
+	def __init__(self, ip = '192.168.1.2', user_name = 'guest'):
+        "New Hue control with ip and user name"
+	    self.ip        = ip
+        self.user_name = user
+        self.bridge    = Bridge(device={'ip':ip}, user={'name':user_name})
+        
+    #ideally this has already been configured    
+    def create_config():
+        "Configures the Hue bridge"
+        created = False
+        print 'Press the button on the Hue bridge'
+
+        # Looks for hue, continually sending message until successfull
+        while not created:
+            # Test message to the hue                                                              
+            resource = {'user':{'devicetype': 'beautifulhuetest', 'name': user_name}}    
+            # Attempt config with message
+            response = self.bridge.config.create(resource)['resource']                       
+            if 'error' in response[0]:                                                  
+                if response[0]['error']['type'] != 101:                        
+                    raise RuntimeError('Unhandled error creating configuration on the Hue')                                                           
+            else:                                                                       
+                created = True                                                          
+
+    def get_system_data():
+        "Return a dict object containing system information"
+        resource = {'which':'system'}                                                     
+        return self.bridge.config.get(resource)['resource']
+
+    def get_new_lights():
+        "Returns a dict object containing all newly added hue lights"
+        resource = {'which':'new'}
+        return self.bridge.light.get(resource)['resource']
+
+    def get_all_lights():
+        "Returns a dict object containing all hue lights"
+        resource = {'which':'all'}
+        return self.bridge.light.get(resource)['resource']
+
+    def get_light(lightIndex):
+        "Returns a dict object containing values of particular light"
+        resources = {'which':lightIndex}
+        return self.bridge.light.get(resource)['resource']
+
+    def find_new_lights():
+        "Discovers new lights associated with bridge"
+        resource = {'which':'new'}
+        return self.bridge.light.find(resource)['resource']   
+
+    def update_light_attributes(whichLights, data):
+        "Updates light attributes such as name, etc"
+        resource = {
+            'which': whichLights
+            # Data is an object in the form {'name', etc}
+            'data' : {'attr':data}
+        } 
+        bridge.light.update(resource)
+
+    def update_light_state(whichLights):
+        "Updates light state such as on, brightness, hue, etc"
+        resource = {
+            'which': whichLights
+            # Data is an object in the form {'on', 'ct', 'bri', etc}
+            'data' : {'state':data}
+        } 
+        bridge.light.update(resource)
---
+++ hue_control/hue_light.py
@@ -0,0 +1,37 @@
+from hue_control import HueBridge
+
+# Controls for a group of one or more lights within the system
+class HueLight:
+
+	def __init__(self, bridge, light = '1'):
+		"Instantiates control over properties of light"
+		if isInstance(bridge, HueBridge):
+			self.bridge = bridge
+		else:
+			print('No bridge provided, assuming default bridge')
+			self.bridge = HueBridge()
+
+		self.light = light
+		
+	def get_info():
+		"Returns a dict holding info about this light/lights"
+		return self.bridge.get_light(self.light)
+
+	def rename(name):	
+		"Changes name of light in Hue system to that passed in"
+		self.bridge.update_light_attributes(self.light, {'name':name})
+
+	def on(color = '232', brightness = '200'):
+		"Turns light on at brightness and color passed in"
+		resource = {'state':'on', 'ct':color, 'bri':brightness}
+		self.bridge.update_light_state(self.light, resource)  
+
+	def update_state(state, color, brightness):
+		"Updates state information to brightness and color passed in"
+		resource = {'state':state, 'ct':color, 'bri':brightness}
+		self.bridge.update_light_state(self.light, resource)		
+
+	def off():
+		"Turns light off"	
+		resource = {'state':'off'}
+		self.bridge.update_light_state(self.light, resource)				
