f98361b2aed5d0037b009e5c7a87087f0ae7a8e5
bob <sczarnecki@lbl.gov>
2016-12-05 16:18:49 -0800

    Adding Copyright Notice to start of each file

--- laptop_power_control/rest_server.py
+++ laptop_power_control/rest_server.py
@@ -1,3 +1,15 @@
+################################################################################################################################
+# *** Copyright Notice ***
+#
+# "Price Based Local Power Distribution Management System (Local Power Distribution Manager) v1.0" 
+# Copyright (c) 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory 
+# (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+# If you have questions about your rights to use or distribute this software, please contact 
+# Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
+################################################################################################################################
+
+
 # TUTORIAL CODE for restful API server, TO BE ADAPTED FOR OUR PURPOSES
 # from https://github.com/miguelgrinberg/REST-tutorial which is MIT licensed as of 20161205:  https://github.com/miguelgrinberg/REST-tutorial/blob/master/LICENSE
 
