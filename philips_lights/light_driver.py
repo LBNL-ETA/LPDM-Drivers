3392e1e00df10174365954f8ab4dfa012598db01
Stephen Czarnecki <sczarnecki@lbl.gov>
2018-03-29 13:51:58 -0400

    Adding low and high switch points to lights.  Also changing lights to act on power availability instead of giving it light level directly.

--- philips_lights/light_driver.py
+++ philips_lights/light_driver.py
@@ -1,12 +1,17 @@
 import requests
+from datetime import datetime, timedelta
 
 class Light_Driver(object):
-    def __init__(self, url, auth, switch_point = 0.50):
+    def __init__(self, url, auth, switch_point_low = 0.05, switch_point_high = 0.50):
         self.url = url
         self.auth = auth
         self.command_template = "<?xml version=\"1.0\" encoding=\"utf-8\"?><LG_CMD><CMD><CMDID>SET_MODE_BC</CMDID><MODE>{mode}</MODE></CMD></LG_CMD>\n\n"
-        self.switch_point = switch_point
-        
+        self.switch_point_low = switch_point_low
+        self.switch_point_high = switch_point_high
+        self.last_set_time = datetime.now() - timedelta(minutes=10)
+        self.time_between_commands = timedelta(minutes=5)
+        self.last_command = None
+    
     def get_charge_command(self):
         return self.command_template.format(mode="CHARGE")
     
@@ -33,15 +38,28 @@ class Light_Driver(object):
         cmd = self.get_grid_command()
         return self.send_command(cmd)
     
-    def set_light_level(self, light_level):
+    def set_power_availablity(self, power_level):
         """
-            Expects light_level to be between 0 and 1.
-            If light_level < .50 sets lights to discharge
-            Otherwise sets lights to charge.
-        """
-        if light_level < self.switch_point:
-            return self.set_to_discharge()
+            Expects power_level to be between 0 and 1.
+            Light is set to charge if the level is below the low switch point
+            Set to grid if between the two points and set to discharge
+            if above the high switch point
+        """        
+        command_to_run = None        
+        if power_level < self.switch_point_low:
+            command_to_run = self.set_to_charge
+        elif self.switch_point_low <= power_level <= self.switch_point_high:
+            command_to_run = self.set_to_grid
         else:
-            return self.set_to_charge()
+            command_to_run = self.set_to_discharge
+        
+        now = datetime.now()
+        
+        if ((now - self.last_set_time) < self.time_between_commands) or (command_to_run == self.last_command):
+            return
+            
+        self.last_command = command_to_run
+        self.last_set_time = now
+        return command_to_run()
             
         
